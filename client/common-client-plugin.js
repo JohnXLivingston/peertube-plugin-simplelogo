ee4154068b57fdd63057b13c6d209467b3337d63
John Livingston <git@john-livingston.fr>
2019-08-20 00:12:24 +0200

    Icon and Favicon url.

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,5 +1,28 @@
 function register ({ registerHook, peertubeHelpers }) {
-
+  peertubeHelpers.getSettings().then(
+    s => {
+      if ( !s ) {
+        return
+      }
+      if ( s['icon_url'] ) {
+        const style = document.createElement('style')
+        document.head.appendChild(style)
+        const sheet = style.sheet
+        let rule = "#custom-css .icon.icon-logo { "
+        rule+= " display: inline-block; "
+        rule+= " background: url('"+s['icon_url']+"'); "
+        if ( s['icon_width'] ) {
+          rule+= " width: "+s['icon_width']+"; "
+        }
+        rule+= " }"
+        sheet.insertRule(rule, 0)
+      }
+      if ( s['favicon_url']) {
+        const favicon = document.querySelector('link[rel="icon"]')
+        favicon.href = s['favicon_url']
+      }
+    }
+  )
 }
 
 export {
